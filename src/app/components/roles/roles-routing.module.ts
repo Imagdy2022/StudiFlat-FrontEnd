4163169ad70539a69ddc41b9ca14f4d89479fe38
Nada Ayman <76060283+nadaayman20@users.noreply.github.com>
2024-05-10 20:19:54 +0300

    Fixed side bar and routing

--- src/app/components/roles/roles-routing.module.ts
+++ src/app/components/roles/roles-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RolesComponent } from './roles.component';
-
+import { AuthorizationComponent } from '../authorization/authorization.component';
 
 
 
 
 const routes: Routes = [
-  { path: '', component: RolesComponent }
+  { path: '', component: RolesComponent },
+  {path: 'authorization/:id', component: AuthorizationComponent }
 ];
 
 @NgModule({
